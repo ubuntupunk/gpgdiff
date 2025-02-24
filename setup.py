187c7e7b3bb532f5018fce55f4245d66fe33a21f
David Robert Lewis <ubuntupunk@gmail.com>
2025-02-24 13:15:25 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
-    name="gpg-prompt",
-    version="0.1.1",
+    name="gpgdiff",
+    version="0.1.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    install_requires=[],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pytest-cov",
-        ]
-    },
+    install_requires=[
+       'rich',
+       'difflib',
+       'argparse',
+       're',
+       'subprocess',
+       'tempfile',
+        ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubuntupunk/gpgdiff",
+    author="David Robert Lewis",
+    author_email="ubuntupunk@gmail.com",
+    keywords='revoke, gpg, diff',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        ],
     entry_points={
     'console_scripts': [
             'gpgdiff=gpg_diff.gpgdiff:main',
         ],
-    },
-    package_data={
-        'gpgdiff': [],
-    },
-    author="ubuntupunk",
-    author_email="ubuntupunk@gmail.com",
-    description="A gpg diff utility",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/ubuntupunk/gpgdiff",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-    ],
+     },   
     python_requires=">=3.6",
 )
